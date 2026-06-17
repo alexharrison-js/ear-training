b0368182f23e5c47c1856d352ba2c9055b6d9bde
alexharrison.js <alexanderjosephharrison@gmail.com>
2026-06-17 10:04:59 -0700

    updates

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import EarTrainer from "./EarTrainer";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <EarTrainer />
-  </React.StrictMode>
-);
