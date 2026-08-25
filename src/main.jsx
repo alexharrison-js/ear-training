78e8712388d3ed491eb8af90b166610c6bcaaf16
Alex Harrison <alexanderjosephharrison@gmail.com>
2026-08-25 00:48:25 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import EarTrainer from "./EarTrainer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <EarTrainer />
   </React.StrictMode>
 );
